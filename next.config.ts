ee40f7d79d9441a7efe37ad57f6d8d8d51a7ec2b
HIROYOKOKI <h.yokoki@icloud.com>
2025-08-21 18:53:44 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // ← これで “workspace root を誤認” 警告を抑止
+  turbopack: { root: __dirname },
+}
 
-export default nextConfig;
+export default nextConfig

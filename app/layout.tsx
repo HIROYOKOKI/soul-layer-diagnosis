b65d7014bbfd537da5a7b1de357239f174ba27c0
HIROYOKOKI <h.yokoki@icloud.com>
2025-08-23 19:30:46 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+// app/layout.tsx
 import './globals.css';
-export default function RootLayout({children}:{children:React.ReactNode}) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <body className="min-h-screen bg-black text-white">{children}</body>

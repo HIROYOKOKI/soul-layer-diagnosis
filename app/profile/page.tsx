d7e733fb32112998f19045a51ca3e62309c10d00
HIROYOKOKI <h.yokoki@icloud.com>
2025-08-24 12:26:04 +0900

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,5 @@
 // app/profile/page.tsx
 'use client'
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
 
 import { useState, type FormEvent, type CSSProperties } from 'react'
 

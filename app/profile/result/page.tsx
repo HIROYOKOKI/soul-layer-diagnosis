a17733abb6d96636f56eda1dee5260e08aa7d446
HIROYOKOKI <h.yokoki@icloud.com>
2025-08-29 12:14:56 +0900

    Update page.tsx

--- app/profile/result/page.tsx
+++ app/profile/result/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 type Result = {
   id: string

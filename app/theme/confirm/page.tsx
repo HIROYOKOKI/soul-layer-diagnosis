12a0502503b62500bf7c1e9a6cd7ccb355cded9d
HIROYOKOKI <h.yokoki@icloud.com>
2025-08-30 22:21:20 +0900

    Update page.tsx

--- app/theme/confirm/page.tsx
+++ app/theme/confirm/page.tsx
@@ -1,6 +1,7 @@
 // app/theme/confirm/page.tsx
 import ConfirmClient from "./ConfirmClient";
 
-export default function ThemeConfirmPage() {
-  return <ConfirmClient />;
+export default function ConfirmPage() {
+  return null; 
 }
+

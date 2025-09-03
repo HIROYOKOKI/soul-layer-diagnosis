6a6150fa06fef46e1eed46cb299764f048067856
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-03 11:30:20 +0900

    Update page.tsx

--- app/daily/confirm/page.tsx
+++ app/daily/confirm/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import AppHeader from '@/app/components/AppHeader'
 import AppSubHeaderTheme from '@/app/components/AppSubHeaderTheme'
 import type { StructureCode } from '@/app/_data/characters/lunea'
 

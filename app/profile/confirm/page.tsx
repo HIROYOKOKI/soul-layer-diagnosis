eadd2944b335116218515d182309a72fec19574c
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-03 16:58:37 +0900

    Update page.tsx

--- app/profile/confirm/page.tsx
+++ app/profile/confirm/page.tsx
@@ -1,10 +1,3 @@
 // app/profile/confirm/page.tsx
-import ConfirmClient from './ConfirmClient'
-
-export const metadata = {
-  title: '選択の確認 | Soul Layer',
-}
-
-export default function Page() {
-  return <ConfirmClient />
-}
+import ConfirmClient from "./ConfirmClient"
+export default function Page(){ return <ConfirmClient /> }

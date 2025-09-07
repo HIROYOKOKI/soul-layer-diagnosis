a9e9704f6bb4ce538b361c5bb9d1740e1d3c0e9e
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-07 22:02:16 +0900

    Update page.tsx

--- app/structure/quick/page.tsx
+++ app/structure/quick/page.tsx
@@ -1,16 +1,5 @@
-// app/structure/quick/page.tsx
-import { Suspense } from 'react'
-import QuickClient from './QuickClient'
-
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
+import ProfileResultClient from "@/components/result/ProfileResultClient"
 
 export default function Page() {
-  return (
-    <Suspense fallback={
-      <div className="min-h-screen bg-black text-white flex items-center justify-center">Loading...</div>
-    }>
-      <QuickClient />
-    </Suspense>
-  )
+  return <ProfileResultClient />
 }

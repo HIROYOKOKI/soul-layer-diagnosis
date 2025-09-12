d43b186b7f6bcba671fc443a18a83a0c37d6b9e2
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-12 14:19:03 +0900

    Update SupabaseProvider.tsx

--- components/SupabaseProvider.tsx
+++ components/SupabaseProvider.tsx
@@ -1,15 +1,21 @@
 // components/SupabaseProvider.tsx
 "use client";
+
+import { useState } from "react";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { useState } from "react";
 import type { Session } from "@supabase/supabase-js";
 
 export default function SupabaseProvider({
   children,
   session,
-}: { children: React.ReactNode; session: Session | null }) {
+}: {
+  children: React.ReactNode;
+  session: Session | null;
+}) {
+  // ブラウザで一度だけクライアントを生成
   const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={session}>
       {children}

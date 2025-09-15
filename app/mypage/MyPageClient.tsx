b9eed3e139e05ba62ab4573ab249b9cbde544200
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-15 09:54:54 +0900

    Update MyPageClient.tsx

--- app/mypage/MyPageClient.tsx
+++ app/mypage/MyPageClient.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
-import RadarEVAE from "@/components/RadarEVAE";
+import { useRouter, useSearchParams } from "next/navigation"
 import Image from "next/image"
 import {
   RadarChart,

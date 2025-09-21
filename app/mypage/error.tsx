4528b6224e6606b2e1498a0e6d2e53da48184b9a
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-21 19:55:08 +0900

    Create error.tsx

---
+++ app/mypage/error.tsx
@@ -0,0 +1,5 @@
+// app/mypage/error.tsx
+'use client'
+export default function Error({ error }: { error: Error }) {
+  return <pre className="p-4 text-red-400 whitespace-pre-wrap">/mypage error: {String(error?.message || error)}</pre>
+}

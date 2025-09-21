65deda1e38f8ba97e60efed85eb984eab3ab5eee
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-21 20:47:41 +0900

    Update layout.tsx

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -1,5 +1,16 @@
-import MyPageClient from "./MyPageClient"
+'use client'
+
+import MyPageShell from '@/components/layout/MyPageShell'
+import DataBridge from '@/components/layout/DataBridge'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 
 export default function Page() {
-  return <MyPageClient />
+  return (
+    <DataBridge demo={false}>
+      <MyPageShell />
+    </DataBridge>
+  )
 }

eebc25f00d47d0549e08c816195741fce1052a3f
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-21 21:37:27 +0900

    Update error.tsx

--- app/mypage/error.tsx
+++ app/mypage/error.tsx
@@ -4,8 +4,7 @@ export default function Error({ error }: { error: Error }) {
   return (
     <pre className="p-4 text-red-400 whitespace-pre-wrap">
       /mypage error: {String(error?.message || 'unknown')}
-      {'\n'}
-      {error?.stack}
+      {'\n'}{error?.stack}
     </pre>
-  )
+  );
 }

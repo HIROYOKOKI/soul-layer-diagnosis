ebfb79c0c7350b87289b806b6d4a4f149d832b96
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-21 18:15:30 +0900

    Update page.tsx

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,6 +3,9 @@
 import MyPageShell from '@/components/layout/MyPageShell'
 import DataBridge from '@/components/layout/DataBridge'
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Page() {
   return (
     <DataBridge demo={false}>

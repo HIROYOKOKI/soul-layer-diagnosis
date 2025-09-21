03549377c3354b5ea334e7b8c4b3f7892a6bc54b
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-21 21:16:13 +0900

    Update page.tsx

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,9 +1,9 @@
+// app/mypage/page.tsx
 'use client'
 
 import MyPageShell from '@/components/layout/MyPageShell'
 import DataBridge from '@/components/layout/DataBridge'
 
-// ✅ 静的化を禁止
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 export const fetchCache = 'force-no-store'

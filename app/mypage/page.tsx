d44de8357a3633597d8d0395827bd104621128e6
HIROYOKOKI <h.yokoki@icloud.com>
2025-09-21 21:49:12 +0900

    Update page.tsx

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,7 +3,9 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
 
-import MyPageClient from './MyPageClientWrapper';
+import MyPageShell from '../../components/layout/MyPageShell'
+import DataBridge from '../../components/layout/DataBridge'
+
 
 export default function Page() {
   return <MyPageClient />;

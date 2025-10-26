eb6a2d865c6a216a97ee0fe3413c38f370bb4ff1
Hiro Yokoki <h.yokoki@icloud.com>
2025-10-26 16:12:28 +0900

    chore: remove stray file

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // app/mypage/page.tsx
 import MyPageClientWrapper from "./MyPageClientWrapper";
 import { headers } from "next/headers";
@@ -64,4 +65,21 @@ export default async function MyPagePage() {
       </div>
     </section>
   );
+=======
+'use client'
+
+import MyPageShell from '@/components/layout/MyPageShell'
+import DataBridge from '@/components/layout/DataBridge'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+
+export default function Page() {
+  return (
+    <DataBridge demo={false}>
+      <MyPageShell />
+    </DataBridge>
+  )
+>>>>>>> a27a9f6 (chore: remove stray file)
 }
